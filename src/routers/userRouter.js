97ac0ff0ac7f9ddd59b677bd9ee905593cb4f3ec
Chloe Lee <chloelee00516@gmail.com>
2023-01-06 10:02:47 +0100

    .
    .

--- src/routers/userRouter.js
+++
@@ -1,9 +0,0 @@
-import { editUser, deleteUser } from "../controllers/userController";
-
-const express = require("express");
-const userRouter = express.Router();
-
-userRouter.get("/edit", editUser);
-userRouter.get("/delete", deleteUser);
-
-export default userRouter;
